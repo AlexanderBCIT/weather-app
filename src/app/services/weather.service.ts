f01dfa5d533c35c7d28dd0c5d393f0bc060e95b6
AlexanderBCIT <xanderbgrahamit@gmail.com>
2026-06-29 19:57:36 +0200

    service generated and base

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class WeatherService {
+  private apiKey = environment.weatherApiKey;
+  private baseUrl = 'https://api.openweathermap.org/data/2.5';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/weather?q=${city}&appid=${this.apiKey}&units=metric`
+    );
+  }
+
+  getForecast(city: string): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/forecast?q=${city}&appid=${this.apiKey}&units=metric`
+    );
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  weatherApiKey: '37946ca73796a396d965d1c733132111'
+};
